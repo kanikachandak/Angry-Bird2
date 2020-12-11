99e89c87ded9903f8dcc432459fcff0d60fda7eb
Kanika <kanikachandak7@gmail.com>
2020-12-11 19:55:02 +0530

    Band

--- sketch.js
+++ sketch.js
@@ -35,8 +35,8 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
     
-    bird = new Bird(100,100);
-    slingshot= new Slingshot(bird.body,{x:200,y:100});
+    bird = new Bird(130,90);
+    slingshot= new Slingshot(bird.body,{x:130,y:90});
 }
 
 function draw(){
--- slingshot.js
+++ slingshot.js
@@ -10,6 +10,9 @@ class Slingshot
         };
         this.pointB=pointB;
         this.slingshot= Constraint.create(options);
+        this.sling1=loadImage("sprites/sling1.png");
+        this.sling2=loadImage("sprites/sling2.png");
+        this.sling3=loadImage("sprites/sling3.png");
          World.add(world, this.slingshot);
     }
     fly()
@@ -22,7 +25,25 @@ class Slingshot
         {
             var pointA=this.slingshot.bodyA.position;
             var pointB=this.pointB;
-            line(pointA.x, pointA.y, pointB.x, pointB.y); //wire coordinate
+            push();
+            stroke(77,35,14);
+            if(pointA.x<150)
+            {
+                strokeWeight(5);
+                line(pointA.x-25, pointA.y, pointB.x, pointB.y); //wire coordinate
+                line(pointA.x-25, pointA.y, pointB.x+35, pointB.y-3);
+                image(this.sling3, pointA.x-30, pointA.y-10, 15,30);
+            }
+            else
+            {
+                strokeWeight(3);
+                line(pointA.x+25, pointA.y, pointB.x, pointB.y); //wire coordinate
+                line(pointA.x+25, pointA.y, pointB.x+35, pointB.y-3);
+                image(this.sling3, pointA.x+25, pointA.y-10, 15,30);
+            }
+            pop();
         }
+        image(this.sling1,150,60);
+        image(this.sling2,120,60);
     }
 }
